206236a796a73607df553bc80b3017a0eb26e03d
Diogo Antonio <diogomorgadoantonio@gmail.com>
2018-08-02 17:08:47 +0100

    [TEST][WIP] Pequeno teste com com a library Html Agility Pack: recolha das ultimas noticias do site Publico

---
+++ Mocks/FrontEnd/Visual Studio/StrikeSentinel/WebScraper/DummyScraper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace WebScraper
+{
+    class DummyScraper
+    {
+        public static string SITE_NAME = "Publico";
+        public static string SITE_ADDRESS = @"https://www.publico.pt/ultimas/";
+
+        public static List<string> Scrape() {
+
+            List<string> newsArticles = new List<string>();
+
+            string xpathLastArticles = "/html/body/div[1]/div[3]/main/div/div/section/div/div/div[2]/div/section[1]/ul/li[1]";
+            string xpathEachArticle = "//article/div/div/div/h3/a";
+
+            /* PARSE */
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(SITE_ADDRESS);
+
+            /* TRAVERSING */
+            var htmlNodesLatestArticles = htmlDoc.DocumentNode.SelectNodes(xpathLastArticles);
+            foreach (var articles in htmlNodesLatestArticles)
+            {
+                var htmlNodesEachArticle = articles.SelectNodes(xpathEachArticle);
+                foreach (var article in htmlNodesEachArticle) {
+                    newsArticles.Add(article.InnerHtml);
+                }
+            }
+
+            return newsArticles;
+        }
+    }
+}
---
+++ Mocks/FrontEnd/Visual Studio/StrikeSentinel/WebScraper/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Scraper is fetching news from " + WebScraper.DummyScraper.SITE_NAME + "...");
+            foreach (string articleTitle in WebScraper.DummyScraper.Scrape())
+            {
+                Console.WriteLine("[" + articleTitle + "]");
+            }
+            Console.WriteLine("Done!!!");
+            Console.ReadKey(true);
+        }
+    }
+}
