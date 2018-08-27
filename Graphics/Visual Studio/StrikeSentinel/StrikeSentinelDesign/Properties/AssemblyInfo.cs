05ad019f9b6a2ac7e57992d0900b85cf7d710667
Diogo Grunho <dgrunho@gmail.com>
2018-08-27 14:39:23 +0100

    Apagar projetos desnecessários

--- Graphics/Visual Studio/StrikeSentinel/StrikeSentinelDesign/Properties/AssemblyInfo.cs
+++ Graphics/Visual Studio/StrikeSentinel/StrikeSentinelDesign/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StrikeSentinel")]
+[assembly: AssemblyTitle("StrikeSentinelDesign")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StrikeSentinel")]
+[assembly: AssemblyProduct("StrikeSentinelDesign")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
