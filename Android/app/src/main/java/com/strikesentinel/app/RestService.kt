07ed294d13175a87e0f3e6ada06b115e5bf97c15
Diogo Grunho <dgrunho@gmail.com>
2018-09-04 15:09:20 +0100

    Continuar a adionar a referencia ao webservice no android

--- Android/app/src/main/java/com/strikesentinel/app/RestService.kt
+++ Android/app/src/main/java/com/strikesentinel/app/RestService.kt
@@ -1,2 +1,21 @@
 package com.strikesentinel.app
 
+class RestService {
+    private val restAdapter: retrofit.RestAdapter
+    val service: InstituteService
+
+    init {
+        restAdapter = retrofit.RestAdapter.Builder()
+                .setEndpoint(URL)
+                .setLogLevel(retrofit.RestAdapter.LogLevel.FULL)
+                .build()
+
+        service = restAdapter.create(InstituteService::class.java)
+    }
+
+    companion object {
+        //You need to change the IP if you testing environment is not local machine
+        //or you may have different URL than we have here
+        private val URL = "http://strikesentinel.ddns.net/api/"
+    }
+}
